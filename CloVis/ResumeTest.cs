5254b0064e4ca9d2902784d413b7285149a22540
Zengoth <caron.clem@gmail.com>
2017-04-03 10:04:29 +0200

    CV_bidon

--- CloVis/ResumeTest.cs
+++ CloVis/ResumeTest.cs
@@ -16,7 +16,33 @@ namespace CloVis
 
             var nom = new Data<string>("nom", "Clémence");
             var tel = new Data<string>("Tel", "+33");
+			var competences = new ElementList("competences");
+			var info = new ElementList("info")
+					{
 
+						new Data<string>("C", "C", 2),
+						new Data<string>("C++","C++",1),
+						new Data<string>("java","java",1),
+					}
+			competences.Add(info);
+
+			var management = new Data<string>("Business Process", "Business Process", 4);
+			competences.Add(management);
+
+			var langues = new ElementList("langues")
+					{
+
+						new Data<string>("anglais", "anglais", 5),
+						new Data<string>("allemand","allemand",2),
+						new Data<string>("chinois","chinois",1.5),
+					}
+
+			var diplomes = new ElementList("diplomes")
+				{
+
+						new DataDated<string>("Flying Spaghetti Monster degree", "Flyer Spaghetti Monster degree", new DateTime(2017,12,24)),
+						new DataDated<string>("bac","bac",new DateTime(1992,11,14)),
+					}
 
             return CV;
         }
