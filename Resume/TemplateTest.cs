60aeaa13220883ba04af14d3257e63a9114c88c9
Robin Thomas <rthomas9629280@gmail.com>
2017-05-02 10:25:15 +0200

    FontElement in TemplateTest

--- Resume/TemplateTest.cs
+++ Resume/TemplateTest.cs
@@ -20,10 +20,10 @@ namespace Resume
 
             var fonts = new Fonts("Polices_cv")
             {
-                new FontElement("Tahoma", 1,  new Color() { R = 120, G = 120, B = 200, A = 100 }, false, true, true, false),    //italic, gras, souligné
-                new FontElement("Tahoma", 3, new Color() { R = 0, G = 0, B = 150, A = 255 },true, true, false, true),
-                new FontElement("Calibri", 2, new Color() { R = 100, G = 100, B = 200, A = 255 },true, false, false, false),
-                new FontElement("Calibri", 1, new Color() { R = 70, G = 70, B = 200, A = 190 },false, false, false, false)
+                new FontElement("Times New Roman", 7,  new Color() { R = 255, G = 0, B = 0, A = 100 }, false, true, true, false),    //italic, gras, souligné
+                new FontElement("Calibri", 5, new Color() { R = 0, G = 255, B = 0, A = 255 },true, true, false, true),
+                new FontElement("Verdana", 4, new Color() { R = 255, G = 0, B = 255, A = 255 },true, false, false, false),
+                new FontElement("Comic Sans MS", 3, new Color() { R = 70, G = 70, B = 200, A = 190 },false, false, false, false)
         };
             var boite_competences = new BoxText(75, 80, 50, 105, 75, 0, "compétences");
             var boite_coordonnées = new BoxText(5, 40, 20, 200, 60, 10, "coordonnées");
@@ -47,6 +47,7 @@ namespace Resume
             };
 
             template.Layout = new Layout();
+            template.Fonts = fonts;
             template.Layout.AddTextBox(boite_coordonnées);
             template.Layout.AddTextBox(boite_langues);
             template.Layout.AddTextBox(boite_competences);
@@ -69,9 +70,9 @@ namespace Resume
 
             var fonts = new Fonts("Polices_cv")
             {
-                new FontElement("Colibri", 9,  new Color() { R = 120, G = 0, B = 200, A = 100 }, false, true, true, false),    //italic, gras, souligné
-                new FontElement("Colibri", 7, new Color() { R = 0, G = 0, B = 150, A = 255 },true, true, false, true),
-                new FontElement("Tahoma", 6, new Color() { R = 0, G = 100, B = 200, A = 255 },true, false, false, false),
+                new FontElement("Colibri", 9,  new Color() { R = 120, G = 0, B = 0, A = 200 }, false, true, true, false),    //italic, gras, souligné
+                new FontElement("Times New Roman", 7, new Color() { R = 0, G = 0, B = 150, A = 255 },true, true, false, true),
+                new FontElement("Tahoma", 6, new Color() { R = 0, G = 0, B = 0, A = 255 },true, false, false, false),
                 new FontElement("Tahoma", 4, new Color() { R = 170, G = 0, B = 0, A = 190 },false, false, true, false)
             };
 
@@ -90,6 +91,7 @@ namespace Resume
             };
 
             template.Layout = new Layout();
+            template.Fonts = fonts;
             template.Layout.AddTextBox(boite_coordonnées);
             template.Layout.AddTextBox(boite_diplomes);
             template.Layout.AddTextBox(boite_langues);
