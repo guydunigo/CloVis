de55ec0725465e9d0fcb6c6f7ff51f093519ab73
KlerviCS <klervi_bzh@live.fr>
2017-05-12 21:58:49 +0200

    test creation fichier xml

--- CloVis/FileManagement.cs
+++ CloVis/FileManagement.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Resume;
-using ResumeElements;
+using System.Xml;
+using System.IO;
+using System.Security;
+
 
 namespace CloVis
 {
     public class FileManagement
     {
+        public FileManagement()
+        {
+            // a faire
+        }
+        public void create_File()
+        {
+            
+            String filename = "fond.xml";
+           
+            FileStream myFileStream = new FileStream(Path.GetFileName(filename), FileMode.OpenOrCreate);
+
+            /*XmlWriter xmlWriter = XmlWriter.Create( new StringBuilder(filename));
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("users");
+
+            xmlWriter.WriteStartElement("user");
+            xmlWriter.WriteAttributeString("age", "42");
+            xmlWriter.WriteString("John Doe");
+            xmlWriter.WriteEndElement();
+
+            xmlWriter.WriteStartElement("user");
+            xmlWriter.WriteAttributeString("age", "39");
+            xmlWriter.WriteString("Jane Doe");
+
+            xmlWriter.WriteEndDocument();
+            //xmlWriter.Close();
+            */
+        }
     }
 }
