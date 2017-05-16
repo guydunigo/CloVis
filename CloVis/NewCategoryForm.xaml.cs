a138ba2e38a619a7c04f5bfb7d7fd306a3dc9e14
Guillaume GONI <gdn3@live.fr>
2017-05-16 22:00:41 +0200

    NewCategoryForm nearly done

--- CloVis/NewCategoryForm.xaml.cs
+++ CloVis/NewCategoryForm.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ResumeElements;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -22,6 +23,55 @@ namespace CloVis
         public NewCategoryForm()
         {
             this.InitializeComponent();
+        }
+
+        public void Validate(object sender)
+        {
+            if (sender is FrameworkElement fe)
+            {
+                TextBlock txt = new TextBlock();
+
+                if (CatName.Text == "")
+                {
+                    txt.Text = "Veuillez renseigner un nom.";
+                    txt.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                }
+                else if (Index.Find(CatName.Text) != null)
+                {
+                    txt.Text = "Ce nom est déjà utilisé";
+                    txt.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                }
+                else if (Index.Find("Divers") is ElementList el)
+                {
+                    el.Add(new ElementList<Element>(CatName.Text));
+                    CatName.Text = "";
+
+                    txt.Text = "Catégorie ajoutée à Divers.";
+                }
+
+                DisplayFlyout(fe, txt);
+            }
+        }
+
+        public static void DisplayFlyout(FrameworkElement source, UIElement elmt)
+        {
+                var fo = new Flyout()
+                {
+                    Content = elmt
+                };
+
+                fo.ShowAt(source);
+        }
+
+        private void CatAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Validate(sender);
+        }
+
+        private void CatName_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                Validate(sender);
         }
     }
 }
