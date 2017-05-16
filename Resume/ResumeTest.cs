295dfe4f9964e7c56847ec180ca994eeee4cb375
Zengoth <caron.clem@gmail.com>
2017-05-16 10:45:29 +0200

    new Clovis CV
    test

--- Resume/ResumeTest.cs
+++ Resume/ResumeTest.cs
@@ -95,5 +95,115 @@ namespace Resume
 
             return CV;
         }
+
+		public static Resume GetResumeTest2()
+		{
+			Resume CV;
+
+            var nom = new Data<string>("Nom", "Clovis", -1, "", true);
+            var titre = new Data<string>("Titre", "Roi des Francs", -1, "", true);
+            var coordonnees = new ElementList<Element>("Coordonnées")
+            {
+                nom,
+                titre,
+				new Data<string>("Téléphone", "+33660606060", -1, "", true),
+				new Data<string>("Mél", "clovis@enib.fr", -1, "", true),
+                new Data<string>("Adresse", "17 rue de la Réussite \n 75012 Paris", -1, "", true)
+			};
+
+            var objectif= new ElementList<Element>("Objectif")
+            {
+                new ElementList<Element>("Mon Objectif")
+                {
+                    new Data<string>("obj", "Je compte unir les Francs, conquérir le nord de la Loire, puis l'Est, et je vaincrai les Burgondes. Mais je ne cesserai pas, et je m'emparerai du Centre et du Sud Ouest de la gaule !", -1, "",true)
+                }
+            }
+
+			var competences = new ElementList<Element>("Compétences")
+			{
+				new ElementList<Element>("Diplomatie")
+				{
+					new Data<string>("Politique", "Politique", 4, "",true),
+					new Data<string>("Assassinat","Assassinat",3, "",true),
+					new Data<string>("Mariage","Mariage",2, "",true),
+                    new Data<string>("Soudoiement","Soudoiement",2, "",true),
+				},
+				
+			};
+
+			var langues = new ElementList<Element>("Langues")
+			{
+				new Data<string>("Francique", "Francique", 5, "",true),
+				new Data<string>("Français moderne","Français moderne",1, "",true),
+				new Data<string>("Allemand moderne","Allemand moderne",1, "",true),
+			};
+
+			var diplomes = new ElementList<Element>("Dynastie")
+			{
+				new DataDated<string>("Mérovingien", "Mérovingien", new DateTime(466,1,1), null, "Depuis $1(y)$",-1, "",true),
+				
+			};
+
+			var fonts = new Fonts("Polices_cv")
+			{
+				new FontElement("Tahoma", 7, new Color() { R = 0, G = 0, B = 255, A = 255 },true, true, true, true), //ARGB 0 on voit rien, 255 opaque
+                new FontElement("Tahoma", 6, new Color() { R = 0, G = 0, B = 150, A = 255 },false, true, false, true),
+				new FontElement("Tahoma", 5, new Color() { R = 100, G = 100, B = 200, A = 255 },true, false, false, false),
+				new FontElement("Calibri", 5, new Color() { R = 70, G = 70, B = 200, A = 190 })
+			};
+
+            var boite_de_nom = new BoxText(85, 15, 60, 40, 20, 0, "nom");
+            var boite_de_titre = new BoxText(75, 40, 60, 40, 10, 0, "titre");
+            var boite_d_obj = new BoxText(15, 50, 60, 40, 15, 0, "obj");
+			
+			var boite_de_coordonnees = new BoxText(165, 50, 60, 40, 60, 0, "Coordonnées");
+			var boite_de_competences = new BoxText(20, 95, 60, 40, 50, 0, "Diplomatie");
+			var boite_de_langues = new BoxText(150, 95, 60, 40, 50, 0, "Langues");
+			var boite_de_diplomes = new BoxText(15, 160, 60, 40, 15, 0, "Dynastie");
+
+			var fond = new BoxBackground(0, 90, 100, 210, 50)
+			{
+				Color = new Color() { A = 140, G = 255 } //verte un peu transparente
+			}; //boite de fond
+
+            /*var fond2 = new BoxBackground(115, 150, 100, 160, 73, null, -40)
+			{
+				Color = new Color() { A = 190, B = 170, G = 255 } //turquoise ? transparent
+			}; //boite de fond
+
+			var fond3 = new BoxBackground(-15, -4, 100, 700, 65, null, 50)
+			{
+				Color = new Color() { A = 255, B = 200, G = 200, R = 255 }
+			};*/
+
+            boite_de_nom.Element = nom;
+            boite_de_titre.Element = titre;
+            boite_d_obj.Element = objectif;
+			boite_de_competences.Element = competences;
+			boite_de_coordonnees.Element = coordonnees;
+			boite_de_langues.Element = langues;
+			boite_de_diplomes.Element = diplomes;
+
+			CV = new Resume()
+			{
+				Fonts = fonts,
+				Name = "CV 1"
+			};
+
+			CV.Layout = new Layout();
+			CV.Layout.AddBackBox(fond);
+            //CV.Layout.AddBackBox(fond2);
+            //CV.Layout.AddBackBox(fond3);
+
+            CV.Layout.AddTextBox(boite_d_obj);
+            CV.Layout.AddTextBox(boite_de_nom);
+            CV.Layout.AddTextBox(boite_de_titre);
+            CV.Layout.AddTextBox(boite_de_competences);
+			CV.Layout.AddTextBox(boite_de_coordonnees);
+			CV.Layout.AddTextBox(boite_de_langues);
+			CV.Layout.AddTextBox(boite_de_diplomes);
+
+			return CV;
+		}
     }
 }
