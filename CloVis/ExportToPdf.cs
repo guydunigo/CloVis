a378d7c69beb014847fc513963c1cc4d850bd3d1
Robin Thomas <rthomas9629280@gmail.com>
2017-05-23 10:22:21 +0200

    minor changes help + create exportToPdf

---
+++ CloVis/ExportToPdf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Resume;
+using CloVis.Controls;
+using Windows.UI.Xaml;
+
+namespace CloVis
+{
+    public static class ExportToPdf
+    {
+
+        public static bool ExportCV(Resume.Resume cv)
+        {
+            var page = new Resume_Preview() { Resume = cv };
+            
+            return false;
+        }
+        
+    }
+}
+
