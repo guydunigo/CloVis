9445bc5cdb874f5d151d4c7275294f6bbc7815c0
Guillaume GONI <gdn3@live.fr>
2017-05-29 11:29:28 +0200

    StartPage to actual StartPage of the app

--- CloVis/App.xaml.cs
+++ CloVis/App.xaml.cs
@@ -73,7 +73,7 @@ namespace CloVis
                     // Quand la pile de navigation n'est pas restaurée, accédez à la première page,
                     // puis configurez la nouvelle page en transmettant les informations requises en tant que
                     // paramètre
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(StartPage), e.Arguments);
                 }
                 // Vérifiez que la fenêtre actuelle est active
                 Window.Current.Activate();
